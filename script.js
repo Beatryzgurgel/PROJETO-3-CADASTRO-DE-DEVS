e24b0860793794fb098003d71c2afa89b9a17a44
Beatryzgurgel <annabeatryzgomesgurgel@gmail.com>
2025-03-11 08:55:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("orderForm")
+
+form.addEventListener("submit" function (ev) {
+  
+  ev.preventDefault()
+  let techCount = 1;
+
+  document.getElementById('addTechBtn').addEventListener('click'), () => {
+      const techContainer = document.getElementById('techContainer');
+
+      const newTechRow = document.createElement('div');
+      newTechRow.classList.add('techRow');
+      newTechRow.innerHTML = `
+  }
+  )
